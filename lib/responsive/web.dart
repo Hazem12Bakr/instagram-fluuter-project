80cd7294981dd600409ad600f9a0951ae73464e7
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-08 20:41:10 +0200

    crate mobile&web screens

---
+++ lib/responsive/web.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class webScreen extends StatelessWidget {
+  const webScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("web screen "),
+      ),
+    );
+  }
+}
