288176f3aba31cf5a2c834b7681cd14b220dfd1e
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-09 02:25:40 +0200

    using layoutbuilder

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,23 @@
 
 import 'package:flutter/material.dart';
 import 'package:instagram_project/responsive/mobile.dart';
+import 'package:instagram_project/responsive/responsive.dart';
 import 'package:instagram_project/responsive/web.dart';
+
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MobileScreen(),
+      home: Responsive(
+        mymobilescreen: MobileScreen(),
+        mywebscreen: WebScreen(),
+      ),
     );
   }
 }
