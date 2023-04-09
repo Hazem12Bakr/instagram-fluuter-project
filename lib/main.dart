049f571c9b614baeaf663b572e87ef213162cfd3
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-09 02:43:47 +0200

    make theme dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:instagram_project/responsive/mobile.dart';
@@ -14,8 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
       home: Responsive(
         mymobilescreen: MobileScreen(),
         mywebscreen: WebScreen(),
