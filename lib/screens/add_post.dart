8fdba2b0197f32213c9454f5f9c21183d78c154b
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-09 23:54:06 +0200

    adding appbar to the screens

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -1,1 +1,21 @@
+// ignore_for_file: unused_import, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+
+class AddPost extends StatefulWidget {
+  const AddPost({super.key});
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AddPost"),
+      ),
+    );
+  }
+}
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,3 +1,21 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  const Search({super.key});
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search"),
+      ),
+    );
+  }
+}
