8c22708b2a8f6d8764fcfa09582b0f01a93ab25d
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-09 23:23:44 +0200

    add the cupertinotabbar

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MobileScreen extends StatelessWidget {
@@ -12,7 +13,10 @@ class MobileScreen extends StatelessWidget {
         title: Text("Mobile Screen"),
       ),
 
-     // bottomNavigationBar: BottomNavigationBar(items: [],),
+      bottomNavigationBar: CupertinoTabBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: ""),
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: ""),
+      ]),
 
     );
   }
