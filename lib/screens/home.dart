f76d6a0ba84e202dcaa1820eb2cf0305e54c62b8
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-10 22:32:24 +0200

    styling the text

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -40,83 +40,84 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 13),
-            child: Row( 
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 13),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Row(
                   children: [
                     CircleAvatar(
-                  radius: 26,
-                  backgroundImage: NetworkImage(
-                    "https://picsum.photos/250?image=9",
-                  ),
-                ),
-                SizedBox(
-                  width: 17,
-                ),
-                Text(
-                  "hazem a.bakr",
-                  style: TextStyle(fontSize: 15),
-                ),
-                
+                      radius: 26,
+                      backgroundImage: NetworkImage(
+                        "https://picsum.photos/250?image=9",
+                      ),
+                    ),
+                    SizedBox(
+                      width: 17,
+                    ),
+                    Text(
+                      "hazem a.bakr",
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ],
                 ),
-                
-                
                 IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
               ],
             ),
           ),
-         
-         Padding(
-           padding: const EdgeInsets.all(8.0),
-           child: Image.network(
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR601dYfdAM4YZpz6rWo_Ir1u18y8IsbxAyeEZh4_qEjpJ6ZqEfvLSBgdIfdZApND4dAqk&usqp=CAU",
-            fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height *0.35,
-            width: double.infinity,
-           ),
-         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical:14 ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                onPressed: (){},
-                 icon: Icon(Icons.favorite_border),
-                 ),
-        
-              IconButton(
-                onPressed: (){},
-                 icon: Icon(
-                  Icons.comment_outlined,
-                 ),
-                 ),
-              IconButton(
-                onPressed: (){},
-                 icon: Icon(
-                  Icons.send,
-                 ),
-                 ),
-                ],
-              ),
-              IconButton(
-                onPressed: (){},
-                 icon: Icon(
-                  Icons.bookmark_outline,
-                 ),
-                 ),
-        
-        
-            ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.network(
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR601dYfdAM4YZpz6rWo_Ir1u18y8IsbxAyeEZh4_qEjpJ6ZqEfvLSBgdIfdZApND4dAqk&usqp=CAU",
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height * 0.35,
+              width: double.infinity,
+            ),
           ),
-        )
-
-       
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 14),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite_border),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.comment_outlined,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.send,
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.bookmark_outline,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
+            width: double.infinity,
+              child: Text(
+            "10 Likes",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ))
         ],
       ),
     );
