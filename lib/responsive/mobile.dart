3706afcb1324da2ed5fc5c103a483e8276b77ccb
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-10 04:07:40 +0200

    adding appbar image

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print, unused_import
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
