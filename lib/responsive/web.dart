d0c8ee2887543062074b212e9c61c175c085115b
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-11 00:40:54 +0200

    create the web appbar

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_project/shared/colors.dart';
 
 class WebScreen extends StatelessWidget {
   const WebScreen({super.key});
@@ -10,7 +11,49 @@ class WebScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Web Screen "),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.home,
+              color: primaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.add_a_photo,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        backgroundColor: mobileBackgroundColor,
+        title: SvgPicture.asset(
+          "assets/img/Instagram.svg.svg",
+          color: primaryColor,
+          height: 32,
+        ),
       ),
     );
   }
