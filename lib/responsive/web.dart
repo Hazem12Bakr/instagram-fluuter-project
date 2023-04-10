a07ef1032357ab6f56d4637ab9a7f15125274365
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-11 00:50:04 +0200

    dispating home screen in web

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -1,7 +1,11 @@
-// ignore_for_file: prefer_const_constructors, deprecated_member_use
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_project/screens/add_post.dart';
+import 'package:instagram_project/screens/home.dart';
+import 'package:instagram_project/screens/profile.dart';
+import 'package:instagram_project/screens/search.dart';
 import 'package:instagram_project/shared/colors.dart';
 
 class WebScreen extends StatelessWidget {
@@ -57,8 +61,22 @@ class WebScreen extends StatelessWidget {
         ),
       ),
    
-   
-   
+    body: PageView(
+      onPageChanged: (index) {
+      
+      },
+       physics: NeverScrollableScrollPhysics(),
+      //controller: _pageController,
+      children: [
+        Home(),
+        Search(),
+        AddPost(),
+        Center(child: Text("salm alykom")),
+        Profile(),
+        
+      ],
+    ),
+
     );
   }
 }
