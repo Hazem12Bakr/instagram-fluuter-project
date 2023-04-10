7ddbe2eaa5911337b7344456eaba08e68f97e037
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-10 02:15:59 +0200

    remove the mobile appbar

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -20,9 +20,7 @@ class _MobileScreenState extends State<MobileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Mobile Screen"),
-      ),
+      
       bottomNavigationBar: CupertinoTabBar(
           backgroundColor: mobileBackgroundColor,
           onTap: (index) {
