d5826af6aeeb3f26821cf3676487e68dd44a6048
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-10 04:19:47 +0200

    modifing image

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: unused_import, prefer_const_constructors
+// ignore_for_file: unused_import, prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_project/shared/colors.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -15,7 +16,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: SvgPicture.asset("assets/img/insta.svg"),
+        title: SvgPicture.asset("assets/img/Instagram.svg.svg",color:primaryColor ,),
       ),
     );
   }
