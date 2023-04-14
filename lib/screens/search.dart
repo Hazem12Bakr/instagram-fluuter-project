6954a1fe007decc1e932690c08519704da6f20fb
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-14 04:03:51 +0200

    giving bgc to appbar&scaffold

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:instagram_project/shared/colors.dart';
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -13,7 +14,9 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: mobileBackgroundColor,
       appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
         title:TextFormField(
           decoration: const InputDecoration(
             labelText: "Search for a user...",
