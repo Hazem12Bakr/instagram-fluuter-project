980539f518a1d3ecede60bbbb60feda4e3ae9245
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-04-14 04:06:04 +0200

    giving bgc to addpost screen scaffold

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:instagram_project/shared/colors.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({super.key});
@@ -13,9 +14,7 @@ class _AddPostState extends State<AddPost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("AddPost screen"),
-      ),
+      backgroundColor: mobileBackgroundColor,
     );
   }
 }
